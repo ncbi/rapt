535614c1a1014a44cecbdb7a56f7d7b075dfb870
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2019-12-31 10:57:40 -0500

    Added branch name check

--- docker/pgap-standalone/teamcity/build.sh
+++ docker/pgap-standalone/teamcity/build.sh
@@ -1,6 +1,8 @@
-BUILD_TYPE=%teamcity.build.branch%
+#BUILD_TYPE=%teamcity.build.branch%
+#echo "##teamcity[setParameter name='env.PGAP_BUILD_TYPE' value='${BUILD_TYPE}']"
 
-echo "##teamcity[setParameter name='env.PGAP_BUILD_TYPE' value='${BUILD_TYPE}']"
+PGAP_BUILD_TYPE=$(git rev-parse --abbrev-ref HEAD)
+echo $PGAP_BUILD_TYPE
 
 ./build-image.sh $PGAP_BUILD_TYPE
 ./save-image.sh $PGAP_BUILD_TYPE
