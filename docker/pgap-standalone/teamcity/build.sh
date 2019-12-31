85d3307913e854acc075c63ede1bb488d990fe2d
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2019-12-31 11:15:14 -0500

    Added step name comments

--- docker/pgap-standalone/teamcity/build.sh
+++ docker/pgap-standalone/teamcity/build.sh
@@ -1,9 +1,16 @@
+# 1. Determine Build Type
 #BUILD_TYPE=%teamcity.build.branch%
 #echo "##teamcity[setParameter name='env.PGAP_BUILD_TYPE' value='${BUILD_TYPE}']"
 
 PGAP_BUILD_TYPE=$(git rev-parse --abbrev-ref HEAD)
 echo $PGAP_BUILD_TYPE
 
+
+# 2. Generate Container Image
 ./build-image.sh $PGAP_BUILD_TYPE
+
+# 3. Save Container Image
 ./save-image.sh $PGAP_BUILD_TYPE
+
+# 4. Push to hub.docker.com
 ./push-image.sh $PGAP_BUILD_TYPE
