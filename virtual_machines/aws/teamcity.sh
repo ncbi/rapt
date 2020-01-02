1fda3478e924aab606e1b121c7b5eb9308a2dcae
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2020-01-02 14:01:38 -0500

    Added progress messages to teamcity script

--- virtual_machines/aws/teamcity.sh
+++ virtual_machines/aws/teamcity.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -eux
 
+echo "##teamcity[progressMessage 'Validate Config']]"
 packer validate pgapx-dev.json
 
+echo "##teamcity[progressMessage 'Build Image']]"
 packer build -machine-readable pgapx-dev.json | tee build.log
 grep 'artifact,0,id' build.log | rev | cut -f1 -d, | cut -f1 -d: | rev > ami-id.txt
 grep 'cwltool.*whl' build.log | rev | cut -f1 -d/ | rev | cut -f1,2 -d- > cwltool-ver.txt
