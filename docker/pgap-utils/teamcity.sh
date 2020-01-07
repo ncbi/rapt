b5c5c2850a98cde6981d4c64ea0d1a24df4bf8bb
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2020-01-07 10:34:48 -0500

    Removed extra messages

--- docker/pgap-utils/teamcity.sh
+++ docker/pgap-utils/teamcity.sh
@@ -13,6 +13,8 @@ if [ "${BUILD_TYPE}" = "dev" ]; then
 fi
 IMAGE_NAME=${IMAGE_NAME}-${BUILD_TYPE}
 
+set +x
+
 echo "##teamcity[blockOpened name='FetchData' description='Fetch binaries and third party data']"
 ./fetch-data.sh $TARBALL $BRANCH $SVNREV $SVNURL
 echo "##teamcity[blockClosed name='FetchData']"
@@ -22,17 +24,14 @@ echo "##teamcity[blockOpened name='Container' description='Generate Container Im
 ./save-image.sh $IMAGE_NAME
 echo "##teamcity[blockClosed name='Container']"
 
-echo "##teamcity[progressMessage 'Archive input-links']"
 echo "##teamcity[blockOpened name='Archive' description='Archive input-links']"
 tar cvzf input-links.tgz input-links
 echo "##teamcity[blockClosed name='Archive']"
 
-echo "##teamcity[progressMessage 'Materialize Data']"
 echo "##teamcity[blockOpened name='Materialize' description='Materialize Data']"
 ./materialize-data.sh
 echo "##teamcity[blockClosed name='Materialize']"
 
-echo "##teamcity[progressMessage 'Copy data to S3']"
 echo "##teamcity[blockOpened name='S3' description='Copy data to S3']"
 ./upload-data.sh $BUILD_TYPE
 echo "##teamcity[blockClosed name='S3']"
