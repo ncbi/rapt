0d7cfcb83b5a75d7de68a4231f45acdf96f35225
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2020-02-05 10:22:54 -0500

    Remove unneeded file creation, fix teamcity messages.

--- docker/pgap-standalone/teamcity.sh
+++ docker/pgap-standalone/teamcity.sh
@@ -6,10 +6,16 @@ echo "##teamcity[progressMessage 'Determine Build Type']]"
 PGAP_BUILD_TYPE=$BRANCH
 
 echo "##teamcity[progressMessage 'Generate Container Image']]"
+echo "##teamcity[blockOpened name='buildImage' description='Generate Container Image']"
 ./build-image.sh $PGAP_BUILD_TYPE
+echo "##teamcity[blockClosed name='buildImage']"
 
 echo "##teamcity[progressMessage 'Save Container Image']]"
+echo "##teamcity[blockOpened name='saveImage' description='Save Container Image']"
 ./save-image.sh $PGAP_BUILD_TYPE
+echo "##teamcity[blockClosed name='saveImage']"
 
 echo "##teamcity[progressMessage 'Push to hub.docker.com']]"
+echo "##teamcity[blockOpened name='pushImage' description='Push to hub.docker.com']"
 ./push-image.sh $PGAP_BUILD_TYPE
+echo "##teamcity[blockClosed name='pushImage']"
