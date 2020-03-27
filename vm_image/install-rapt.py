e4d79030a9563e1b1d884961c9c2199114652d94
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2020-03-27 16:07:20 -0400

    Remove branches, add guard files, add standalone docker image

--- vm_image/install-rapt.py
+++ vm_image/install-rapt.py
@@ -4,43 +4,35 @@ import os
 import subprocess
 
 version=sys.argv[1]
-branch=sys.argv[2]
-is_release=branch not in ["dev", "test", "prod"]
 
 os.umask(0o002)
 
 # Get CWL code
-if is_release:
-    os.makedirs("cwl", exist_ok=True)
-    remote_file=f"https://api.github.com/repos/ncbi/pgap/tarball/{version}"
-    subprocess.run(["curl", "-sLO", remote_file], check=True)
-    subprocess.run(["tar", "xvzf", version, "-C", "cwl", "--strip-components=1"], check=True)
-    os.remove(version)
-else:
-    subprocess.run(["git", "clone", "--branch", branch, "https://github.com/ncbi/pgap.git", "cwl"], check=True)
+os.makedirs("cwl", exist_ok=True)
+remote_file=f"https://api.github.com/repos/ncbi/pgap/tarball/{version}"
+subprocess.run(["curl", "-sLO", remote_file], check=True)
+subprocess.run(["tar", "xvzf", version, "-C", "cwl", "--strip-components=1"], check=True)
+os.remove(version)
 
 subprocess.run(["ln", "-s", "cwl/scripts/pgap.py"], check=True)
     
-# Get Docker image
-if is_release:
-    repo="pgap-utils"
-else:
-    repo=f"pgap-{branch}"
-image=f"ncbi/{repo}:{version}"
-subprocess.run(["sudo", "docker", "pull", "-q", image], check=True)
+# Get PGAP docker images
+repos = [ "pgap-utils" , "pgap" ]
+for repo in repos:
+    image=f"ncbi/{repo}:{version}"
+    subprocess.run(["sudo", "docker", "pull", "-q", image], check=True)
+
+# Get Skesa
 subprocess.run(["sudo", "docker", "pull", "-q", "ncbi/skesa:v2.3.0"], check=True)
 subprocess.run(["sudo", "rm", "-rf", ".docker"], check=True)
 
 # Get input data
-if is_release:
-    url=f"https://s3.amazonaws.com/pgap/input-{version}.tgz"
-else:
-    url=f"https://s3.amazonaws.com/pgap/input-{version}.{branch}.tgz.tgz"
+url=f"https://s3.amazonaws.com/pgap/input-{version}.tgz"
 subprocess.run([f"curl -s {url} | tar xvzf -"], shell=True, check=True)
+open(f'input-{version}/.pgap_complete', 'a').close()
 
 # Get ANI data
-if is_release:
-    url=f"https://s3.amazonaws.com/pgap/input-{version}.ani.tgz"
-else:
-    url=f"https://s3.amazonaws.com/pgap/input-{version}.{branch}.ani.tgz"
+url=f"https://s3.amazonaws.com/pgap/input-{version}.ani.tgz"
 subprocess.run([f"curl -s {url} | tar xvzf -"], shell=True, check=True)
+open(f'input-{version}/.ani_complete', 'a').close()
+
