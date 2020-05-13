b4c453b3fa2775d9c503a593575287a2846906d3
Badretdin, Azat <badrazat@ncbi.nlm.nih.gov>
2020-05-13 08:14:17 -0400

    less wrong account; JIRA; PGAPX-776

--- docker/pgap-utils/upload-data-GCP.sh
+++ docker/pgap-utils/upload-data-GCP.sh
@@ -4,7 +4,7 @@
 #
 
 set -uexo pipefail
-key_file=/panfs/pan1/gpipe/etc/.gpipe_gcp/ncbi-pgapx-dev-2676637e00f6.json
+key_file=/panfs/pan1/gpipe/etc/.gpipe_gcp/ncbi-pgapx-f16c4ab8be62.json
 # project=ncbi-pgapx
 project=$(grep '"project_id"' "$key_file" | cut -d'"' -f4)
 gcloud auth activate-service-account --project="$project" --key-file="$key_file"
