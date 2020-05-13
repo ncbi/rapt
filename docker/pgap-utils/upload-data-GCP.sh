2f3b01e657d0737e5d629a9dee20521c442fe74b
Badretdin, Azat <badrazat@ncbi.nlm.nih.gov>
2020-05-13 08:08:36 -0400

    added gcloud authentication command; JIRA; PGAPX-776

--- docker/pgap-utils/upload-data-GCP.sh
+++ docker/pgap-utils/upload-data-GCP.sh
@@ -4,6 +4,10 @@
 #
 
 set -uexo pipefail
+key_file=/panfs/pan1/gpipe/etc/.gpipe_gcp/ncbi-pgapx-dev-2676637e00f6.json
+# project=ncbi-pgapx
+project=$(grep '"project_id"' "$key_file" | cut -d'"' -f4)
+gcloud auth activate-service-account --project="$project" --key-file="$key_file"
 tarballs_file=tarballs.for.upload
 pids=
 for tarfile in $(cat "$tarballs_file" /dev/null); do
