4774e8e71eb8e5abca536d2c28f2c7b40e2d4267
Lukas Wagner <wagner@genome1.be-md.ncbi.nlm.nih.gov>
2020-05-14 14:19:41 -0400

    rules set in loop

--- docker/skesa/cloudbuild/misc_util/blocking_firewall.sh
+++ docker/skesa/cloudbuild/misc_util/blocking_firewall.sh
@@ -3,35 +3,19 @@
 #gcp vm execution requires auth login.  
 #host:port info https://github.com/ncbi/sra-tools/wiki/Firewall-and-Routing-Information
 
-gcloud compute firewall-rules create block-egress-lo-tcp-www  --action allow  --rules tcp --destination-ranges 130.14.29.110/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-tcp-www --action allow  --rules tcp --source-ranges 130.14.29.110/32 --direction=INGRESS --priority 60000
+gcloud compute firewall-rules create block-egress-lo-tcp-www  --action deny  --rules tcp --destination-ranges 130.14.29.110/32 --direction=EGRESS --priority 60000
+gcloud compute firewall-rules create block-ingress-lo-tcp-www --action deny  --rules tcp --source-ranges 130.14.29.110/32 --direction=INGRESS --priority 60000
 
-gcloud compute firewall-rules create block-egress-lo-udp-24 --action deny  --rules udp --destination-ranges 130.14.250.24/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-egress-lo-tcp-24 --action deny  --rules tcp --destination-ranges 130.14.250.24/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-udp-24 --action deny  --rules udp --source-ranges 130.14.250.24/32 --direction=INGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-tcp-24 --action deny  --rules tcp --source-ranges 130.14.250.24/32 --direction=INGRESS --priority 60000
+for ip in 130.14.250.{24,25,26,27} 165.112.9.{231,232} 
 
-gcloud compute firewall-rules create block-egress-lo-udp-25 --action deny  --rules udp --destination-ranges 130.14.250.25/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-egress-lo-tcp-25 --action deny  --rules tcp --destination-ranges 130.14.250.25/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-udp-25 --action deny  --rules udp --source-ranges 130.14.250.25/32 --direction=INGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-tcp-25 --action deny  --rules tcp --source-ranges 130.14.250.25/32 --direction=INGRESS --priority 60000
+   do
+      echo ${ip}
+      IFS=.  
+      read ip0 ip1 ip2 ip3 <<< "$ip"  
+      
+      gcloud compute firewall-rules create allow-egress-lo-udp-${ip3} --action deny  --rules udp --destination-ranges ${ip}/32 --direction=EGRESS --priority ${priority}
+      gcloud compute firewall-rules create allow-egress-lo-tcp-${ip3} --action deny  --rules tcp --destination-ranges ${ip}/32 --direction=EGRESS --priority ${priority}
+      gcloud compute firewall-rules create allow-ingress-lo-udp-${ip3} --action deny  --rules udp --source-ranges ${ip}/32 --direction=INGRESS --priority ${priority}
+      gcloud compute firewall-rules create allow-ingress-lo-tcp-${ip3} --action deny  --rules tcp --source-ranges ${ip}/32 --direction=INGRESS --priority ${priority}
 
-gcloud compute firewall-rules create block-egress-lo-udp-26 --action deny  --rules udp --destination-ranges 130.14.250.26/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-egress-lo-tcp-26 --action deny  --rules tcp --destination-ranges 130.14.250.26/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-udp-26 --action deny  --rules udp --source-ranges 130.14.250.26/32 --direction=INGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-tcp-26 --action deny  --rules tcp --source-ranges 130.14.250.26/32 --direction=INGRESS --priority 60000
-
-gcloud compute firewall-rules create block-egress-lo-udp-27 --action deny  --rules udp --destination-ranges 130.14.250.27/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-egress-lo-tcp-27 --action deny  --rules tcp --destination-ranges 130.14.250.27/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-udp-27 --action deny  --rules udp --source-ranges 130.14.250.27/32 --direction=INGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-tcp-27 --action deny  --rules tcp --source-ranges 130.14.250.27/32 --direction=INGRESS --priority 60000
-
-gcloud compute firewall-rules create block-egress-lo-udp-231 --action deny  --rules udp --destination-ranges 165.112.9.231/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-egress-lo-tcp-231 --action deny  --rules tcp --destination-ranges 165.112.9.231/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-udp-231 --action deny  --rules udp --source-ranges 165.112.9.231/32 --direction=INGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-tcp-231 --action deny  --rules tcp --source-ranges 165.112.9.231/32 --direction=INGRESS --priority 60000
-
-gcloud compute firewall-rules create block-egress-lo-udp-232 --action deny  --rules udp --destination-ranges 165.112.9.232/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-egress-lo-tcp-232 --action deny  --rules tcp --destination-ranges 165.112.9.232/32 --direction=EGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-udp-232 --action deny  --rules udp --source-ranges 165.112.9.232/32 --direction=INGRESS --priority 60000
-gcloud compute firewall-rules create block-ingress-lo-tcp-232 --action deny  --rules tcp --source-ranges 165.112.9.232/32 --direction=INGRESS --priority 60000
+   done
